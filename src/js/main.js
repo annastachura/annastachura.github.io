aee944c670e210ad9c7fa0cc23637a5a35d02528
Anna Stachura <annas19914@gmail.com>
2019-10-23 16:17:07 +0200

    changes js

--- src/js/main.js
+++ src/js/main.js
@@ -18,7 +18,7 @@ fetch('https://api.github.com/users/annastachura/repos?sort-update')
                     <p class="card__paragraph">${repo.description} ${repo.homepage}</p>
                 </div>
                 <div class="card__background">
-                    <a class="card__link" href="${repo.homepage}" target="_blank" rel ="nofollow noreferer" title="Demo:${repo.name}.">Demo</a>
+                    <a class="card__link" href="https://annastachura.github.io/${repo.name}" target="_blank" rel ="nofollow noreferer" title="Demo:${repo.name}.">Demo</a>
                         <img src="assets/img/demo-icon.png"></a>
                     <a class="card__link" href="${repo.html_url}" target="_blank" rel ="nofollow noreferer" title="Source code"${repo.name}.">Github</a>
                         <img src="assets/img/code-icon.png"></a>
