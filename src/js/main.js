9b253024c8e23fc9f4d7516ff878d26ae619da93
Anna Stachura <annas19914@gmail.com>
2019-10-23 12:13:54 +0200

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -8,17 +8,17 @@ fetch('https://api.github.com/users/annastachura/repos?sort-update')
     .then(resp => {
         const repos = resp;
         for (const repo of repos) {
-            consile.log(repo);
+            console.log(repo);
             list.innerHTML += `
 
    <li class="card">
                 <div class="card__box">
                     <img class="card__image" src="assets/img/github.png">
-                    <h3 class="card__title">${name}</h3>
-                    <p class="card__paragraph">${description}</p>
+                    <h3 class="card__title">${repo.name}</h3>
+                    <p class="card__paragraph">${repo.description}</p>
                 </div>
                 <div class="card__background">
-                    <a class="card__link" href="#" title="Demo:${name}.">Demo</a>
+                    <a class="card__link" href="#" title="Demo:${repo.name}.">Demo</a>
                         <img src="assets/img/demo-icon.png">Demo</a>
                     <a class="card__link" href="${html_url}" target="_blank" rel ="nofollow noreferer" title="Source code"${name}.">Github</a>
                         <img src="assets/img/code-icon.png">Github</a>
