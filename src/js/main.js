cfe849f643af4ed9696b2e5c6779df115c203ea8
Anna Stachura <annas19914@gmail.com>
2019-10-24 16:21:55 +0200

    css changes

--- src/js/main.js
+++ src/js/main.js
@@ -10,8 +10,7 @@ fetch('https://api.github.com/users/annastachura/repos?sort-update')
         for (const repo of repos) {
             console.log(repo);
             list.innerHTML += `
-
-   <li class="card">
+            <li class="card">
                 <div class="card__box">
                     <img class="card__image" src="assets/img/github.png">
                     <h3 class="card__title">${repo.name}</h3>
@@ -19,9 +18,9 @@ fetch('https://api.github.com/users/annastachura/repos?sort-update')
                 </div>
                 <div class="card__background">
                     <a class="card__link" href="https://annastachura.github.io/${repo.name}" target="_blank" rel ="nofollow noreferer" title="Demo:${repo.name}.">Demo</a>
-                        <img src="assets/img/demo-icon.png"></a>
-                    <a class="card__link" href="${repo.html_url}" target="_blank" rel ="nofollow noreferer" title="Source code"${repo.name}.">Github</a>
-                        <img src="assets/img/code-icon.png"></a>
+                        <img src="assets/img/demo-icon.svg"></a>
+                    <a class="card__link card__link--code" href="${repo.html_url}" target="_blank" rel ="nofollow noreferer" title="Source code"${repo.name}.">Github</a>
+                        <img src="assets/img/code-icon.svg"></a>
                 </div>
             </li>
    `;
